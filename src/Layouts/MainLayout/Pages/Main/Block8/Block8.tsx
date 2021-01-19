6973c4212d48e113b35c96f6bf93c2d3638e1846
Denis <64834314+sanddwich@users.noreply.github.com>
2021-01-19 18:49:53 +0400

    123231

---
+++ src/Layouts/MainLayout/Pages/Main/Block8/Block8.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import './Block8.scss'
+
+interface Block8Props {}
+
+interface Block8State {}
+
+class Block8 extends React.Component<Block8Props, Block8State> {
+  render() {
+    return(
+      <Container className="Block8">
+
+      </Container>
+    )
+  }
+}
+
+export default Block8
