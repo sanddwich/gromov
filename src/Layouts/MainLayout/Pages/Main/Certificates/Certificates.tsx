d0614ba7085fe590a5dcc1aa34c6814f15b738b4
Denis <64834314+sanddwich@users.noreply.github.com>
2021-02-15 20:01:17 +0400

    123322133

--- src/Layouts/MainLayout/Pages/Main/Certificates/Certificates.tsx
+++ src/Layouts/MainLayout/Pages/Main/Certificates/Certificates.tsx
@@ -6,31 +6,86 @@ import { Element } from 'react-scroll'
 import 'swiper/swiper.scss'
 
 import './Certificates.scss'
+import { Container, Row } from 'react-bootstrap'
 
 interface CertificatesProps {}
 
-interface CertificatesState {}
+interface CertificatesState {
+  certificates: string[]
+}
 
 class Certificates extends React.Component<CertificatesProps, CertificatesState> {
+  constructor(props: CertificatesProps) {
+    super(props)
+    this.state = {
+      certificates: [
+        '2.jpeg',
+        '3.jpeg',
+        '4.jpeg',
+        '5.jpeg',
+        '6.jpeg',
+        '7.jpeg',
+        '8.jpeg',
+        '9.jpeg',
+        '10.jpeg',
+        '11.jpeg',
+        '12.jpeg',
+        '13.jpeg',
+        '14.jpeg',
+        '15.jpeg',
+        '16.jpeg',
+        '17.jpeg',
+      ],
+    }
+  }
+
   render() {
     return (
       <Element name="Certificates">
-        <div className="Certificates container-lg">
-          <Swiper spaceBetween={40} slidesPerView={3}>
-            <SwiperSlide>
-              <div className="Certificates__slide"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="Certificates__slide"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="Certificates__slide"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="Certificates__slide"></div>
-            </SwiperSlide>
+        <Container fluid className="Certificates d-none d-md-block p-0">
+          <Swiper
+            slidesPerView={2}
+            // centeredSlides={true}
+            // spaceBetween={20}
+            pagination={{ el: '.swiper-pagination', clickable: true }}
+          >
+            {this.state.certificates.map((cert, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <div className="Certificates__slide d-flex justify-content-center align-items-center">
+                    <img className="img-fluid" src={`/img/cert/${cert}`} alt="" />
+                  </div>
+                </SwiperSlide>
+              )
+            })}
           </Swiper>
-        </div>
+          <Row className="Certificates__pagination m-0 d-flex justify-content-center pt-5">          
+            <div className="swiper-pagination"></div>  
+          </Row>
+        </Container>
+
+        <Container fluid className="Certificates__Mobile d-block d-md-none p-0">
+          <Swiper
+            slidesPerView={1}
+            centeredSlides={true}
+            spaceBetween={10}
+            pagination={{ el: '.swiper-paginationMobile', clickable: true }}
+          >
+            {this.state.certificates.map((cert, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <div className="Certificates__slide d-flex justify-content-center align-items-center">
+                    <img className="img-fluid" src={`/img/cert/${cert}`} alt="" />
+                  </div>
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
+          <Row className="Certificates__pagination d-flex justify-content-center pt-5">          
+            <div className="swiper-paginationMobile"></div>  
+          </Row>
+        </Container>
+
       </Element>
     )
   }
